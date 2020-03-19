facae4d212c01eb7084ce564a9298bbed22d3cbf
Mateusz Kadłubowski <s18784@pjwstk.edu.pl>
2020-03-19 15:08:25 +0100

    commit 2

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APBD_02.Models
+{
+    public class Student
+    {
+
+        public string Imie { get; set; }
+        public string Nazwisko{ get; set; }
+        public string Email { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using APBD_02.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace APBD_02
 {
@@ -7,17 +10,43 @@ namespace APBD_02
     {
         static void Main(string[] args)
         {
-            var inputPath = args.Length > 0 ? args[0] : "Files/dane.csv";
-            var outputPath = args.Length == 3 ? args[0] : "output";
+            var inputPath = args.Length > 0 ? args[0] : @"Files\dane.csv";
+            var outputPath = args.Length == 3 ? args[0] : @"Files\result.xml";
             var dataType= args.Length == 3 ? args[0] : "xml";
 
             //Console.WriteLine($"{arg1} {arg2} {arg3}");
+            try
+            {
+
+                if (!File.Exists(inputPath)) throw new FileNotFoundException("ERR", inputPath.Split("\\")[^1]); 
 
             var contents = File.ReadAllLines(inputPath);
+                var list = new List<Student>();
 
-            foreach(var line in contents)
+                foreach (var line in contents)
+                {
+                    //Console.WriteLine(line);
+                    File.AppendAllText(outputPath, line);
+                    var splitted = line.Split(",");
+                    if (splitted.Length == 9)
+                    {
+                        var student = new Student
+                        {
+                            Imie = splitted[0],
+                            Nazwisko = splitted[1],
+                            Email = splitted[6]
+                        };
+                        list.Add(student);
+                    }
+                }
+
+                using var writer = new FileStream($"{outputPath}", FileMode.Create);
+                var serializer = new XmlSerializer(typeof(List<Student>));
+                serializer.Serialize(writer, list);
+            }
+            catch(FileNotFoundException e)
             {
-                Console.WriteLine(line);
+                File.AppendAllText("log.txt", $"{DateTime.UtcNow} {e.Message} {e.FileName}");
             }
         }
     }
