e271765bfaab5b4710605f82385d98b67aa5eb8b
Mateusz Kadłubowski <s18784@pjwstk.edu.pl>
2020-03-19 15:07:30 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace APBD_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputPath = args.Length > 0 ? args[0] : "Files/dane.csv";
+            var outputPath = args.Length == 3 ? args[0] : "output";
+            var dataType= args.Length == 3 ? args[0] : "xml";
+
+            //Console.WriteLine($"{arg1} {arg2} {arg3}");
+
+            var contents = File.ReadAllLines(inputPath);
+
+            foreach(var line in contents)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
